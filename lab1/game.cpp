29bcba5c79d4cd7e45ae925a5a3e1bb7f1635fd7
mitrandir-pl <leshkopavel@gmail.com>
2021-10-27 23:45:46 +0300

    show function added

--- lab1/game.cpp
+++ lab1/game.cpp
@@ -50,3 +50,50 @@ void Game::cur_pos_out(int colour) {
 	cout << (char)code;
 	SetConsoleTextAttribute(hStdOut, (WORD)(15));
 }
+
+
+void Game::show_matrix() {
+	for (int i = 0; i < this->field.size(); i++) {
+		for (auto j = 0; j < this->field[i].size(); j++) {
+			cout << this->field[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void Game::show() {
+	system("cls");
+	for (int i = 0; i < this->field.size(); i++) {
+		for (auto j = 0; j < this->field[i].size(); j++) {
+			if (i == this->cur_pos.first && j == this->cur_pos.second) {
+				if (!this->is_empty(this->cur_pos.first, this->cur_pos.second)) {
+					cur_pos_out(12);
+				}
+				else {
+					cur_pos_out(10);
+				}
+				//			if (j == (this->size - 1)) {
+				//				cout << endl;
+				//			}
+				continue;
+			}
+			int code;
+			switch (this->field[i][j]) {
+			case 0:
+				code = 250;
+				break;
+			case 1:
+				code = 120;
+				break;
+			case 2:
+				code = 111;
+				break;
+			}
+			cout << (char)code;
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
